537b451961ab682e39151ecf1424388c22ab1659
Ozair Farahi <ozairfarahi6@gmai.com>
2020-05-04 22:53:29 +0430

    Added User Register Route and validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
 
 // @route   GET api/users
-// @desc    Test Route
+// @desc    Register User
 // @access  Public
 
-router.get('/', (req,res) => res.send('User Route'));
+router.post('/', [
+    check('name', 'Name is required!').not().isEmpty(),
+    check('email', 'Please Include Email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+    res.send('User Route');
+});
+
 
 module.exports = router;
