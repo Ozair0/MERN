cb7ad242e97b641bb6fdea49e650360c529e1691
Ozair Farahi <ozairfarahi6@gmai.com>
2020-05-05 14:22:24 +0430

    Added Experience to Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -126,7 +126,7 @@ router.get('/user/:user_id', async (req, res) => {
 
 // @route   DELETE api/profile
 // @desc    Delete profile, user & posts
-// @access  private
+// @access  Private
 
 router.delete('/', auth, async (req, res) => {
     try {
@@ -142,5 +142,55 @@ router.delete('/', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 })
+
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+
+router.put('/experience', [auth, [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }
+
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
+        if(!profile){
+            return res.status(400).json({msg: 'Profile Not Found'})
+        }
+        profile.experience.unshift(newExp);
+        await profile.save();
+        res.json(profile);
+    }catch (e) {
+        console.error(e.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
 
 module.exports = router;
