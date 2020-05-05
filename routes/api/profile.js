774a7d7fe7b4d945c7d55bd15a64c66a3be8870d
Ozair Farahi <ozairfarahi6@gmai.com>
2020-05-05 13:59:08 +0430

    Added Get Profile by UserID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -90,4 +90,38 @@ router.post('/', [auth, [
     }
 });
 
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+
+router.get('/', async (req,res) => {
+    try{
+        const profiles = await Profile.find().populate('user', ['name','avatar']);
+        res.json(profiles);
+    }catch (e) {
+        console.error(e.message);
+        res.status(500).send('Server Error');
+    }
+})
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+
+router.get('/user/:user_id', async (req,res) => {
+    try{
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name','avatar']);
+        if(!profile){
+            return res.status(400).json({ msg: 'Profile not found' })
+        }
+        res.json(profile);
+    }catch (e) {
+        console.error(e.message);
+        if(e.name === 'CastError'){
+            return res.status(400).json({ msg: 'Profile not found' })
+        }
+        res.status(500).send('Server Error');
+    }
+})
+
 module.exports = router;
